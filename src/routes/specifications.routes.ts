ce4a8936b5c9d627460ad9e373002bff8c8b1430
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-01-25 18:55:50 -0300

    perf(specification): changed create specification function to useCases

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,21 +1,13 @@
 import { Router } from "express";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
 
 const specificationsRoutes = Router();
-
-const specificationsRepository = new SpecificationsRepository
 
 
 
 specificationsRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
-    const createSpecificationService = new CreateSpecificationService(specificationsRepository);
-
-    createSpecificationService.execute({ name, description });
-
-    return response.status(201).send();
+    return createSpecificationController.handle(request, response);
 })
 
 export { specificationsRoutes };
