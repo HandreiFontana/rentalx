e7e7db2ad9e6a3ebd293b87c9db77ba13ed832b2
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-01-25 15:34:01 -0300

    perf(project): directories has changed

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 
-import { CategoriesRepository } from '../repositories/CategoriesRespository';
-import { PostgresCategoriesRepository } from '../repositories/PostgresCategoriesRepository';
-import { CreateCategoryService } from '../services/CreateCategoryService';
+import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRespository';
+
+import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
