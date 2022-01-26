8c5a85666d8319d945cf2411eb1caaf9a44a466a
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-01-26 13:23:04 -0300

    feat(upload): installed multer and csv-parse and created upload file function

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,21 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+
+const csvParse = parse;
+
+class ImportCategoryUseCase {
+    execute(file: Express.Multer.File): void {
+        
+        const stream = fs.createReadStream(file.path);
+
+        const parseFile = csvParse();
+
+        stream.pipe(parseFile);
+
+        parseFile.on("data", async (line) => {
+            console.log(line);
+        });
+    }
+}
+
+export { ImportCategoryUseCase };
