e2f78fef65f2ed7c0eb38172c93612c9200fc743
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-02-08 16:38:02 -0300

    feat(cars): list available cars function created and filter by name, brand, category

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -13,7 +13,7 @@ class CarsRepository implements ICarsRepository {
     constructor() {
         this.repository = getRepository(Car);
     }
-
+    
     async create({ 
         brand,
         category_id,
@@ -32,20 +32,48 @@ class CarsRepository implements ICarsRepository {
             license_plate,
             name, 
         });
-
+        
         await this.repository.save(car);
-
+        
         return car;
     }
-
+    
     async findByLicensePlate(license_plate: string): Promise<Car> {
         const car = await this.repository.findOne({
             license_plate
         })
-
+        
         return car;
     }
 
+    async findAvailable(
+        brand?: string, 
+        category_id?: string, 
+        name?: string
+    ): Promise<Car[]> {
+        
+        const carsQuery = await this.repository
+        .createQueryBuilder("c")
+        .where("available =  :available", { available: true });
+
+        if(brand) {
+            carsQuery.andWhere("brand = :brand", { brand })
+        }
+
+        if(name) {
+            carsQuery.andWhere("name = :name", { name })
+        }
+
+        if(category_id) {
+            carsQuery.andWhere("category_id = :category_id", { category_id })
+        }
+
+        const cars = await carsQuery.getMany();
+
+        return cars;
+
+    }
+    
 }
 
 export { CarsRepository }
--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -5,6 +5,8 @@ import multer from 'multer';
 import { CreateCategoryController } from '@modules/cars/useCases/createCategory/CreateCategoryController';
 import { ImportCategoryController } from '@modules/cars/useCases/importCategory/ImportCategoryController';
 import { ListCategoriesController } from '@modules/cars/useCases/listCategories/ListCategoriesController';
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
+import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
 
 
 const categoriesRoutes = Router();
@@ -17,13 +19,20 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoryController = new ListCategoriesController();
 
-categoriesRoutes.post("/", createCategoryController.handle);
+categoriesRoutes.post(
+    "/",
+    ensureAuthenticated,
+    ensureAdmin,
+    createCategoryController.handle
+);
 
 categoriesRoutes.get("/", listCategoryController.handle);
 
 categoriesRoutes.post(
     "/import", 
-    upload.single("file"), 
+    upload.single("file"),
+    ensureAuthenticated,
+    ensureAdmin,
     importCategoryController.handle
 );
 
