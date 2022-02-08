8f96ff46f1b477e678f84b0174de0371db360740
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-02-08 13:52:17 -0300

    feat(cars): create car function created

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,26 +1,34 @@
-import { container } from 'tsyringe'
+import { container } from 'tsyringe';
 
-import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository"
-import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRespository'
+import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
+import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRespository';
 
-import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationRepository'
-import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationRepository'
+import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationRepository';
+import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationRepository';
 
-import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository'
-import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UserRepository'
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UserRepository';
+
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
     CategoriesRepository
-)
+);
 
 container.registerSingleton<ISpecificationsRepository>(
     "SpecificationsRepository",
     SpecificationsRepository
-)
+);
 
 container.registerSingleton<IUsersRepository>(
     "UsersRepository",
     UsersRepository
-)
+);
+
+container.registerSingleton<ICarsRepository>(
+    "CarsRepository",
+    CarsRepository
+);
