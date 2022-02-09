1c44f37e617fb780cc02f8a07e8a2e7c448c6459
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-02-09 11:21:47 -0300

    feat(rentals): rentals table with migration created

--- src/shared/infra/typeorm/migrations/1643900580906-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1643900580906-CreateUsers.ts
@@ -10,6 +10,7 @@ export class CreateUsers1643900580906 implements MigrationInterface {
                     {
                         name: "id",
                         type: "uuid",
+                        isPrimary: true,
                     },
                     {
                         name: "name",
