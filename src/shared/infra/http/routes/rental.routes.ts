65b6eed264cb352e0c780c2aa80a7c42e122c842
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-02-10 16:36:19 -0300

    feat(rentals): devolution rental car function created

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 
@@ -8,8 +10,14 @@ const rentalRoutes = Router();
 
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
+rentalRoutes.post(
+    "/devolution/:id", 
+    ensureAuthenticated, 
+    devolutionRentalController.handle
+);
 
 
 
