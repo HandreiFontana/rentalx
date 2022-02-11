a79fc603b2868ac67d88824971f2af8341ba9e82
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-02-11 10:58:15 -0300

    feat(refreshToken): refresh token create with auth token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "3de5b9ee05c957ae9c67db35c6f4eb78",
+    expires_in_token: "15m",
+    secret_refresh_token: "cb6d671d9e85efe6fd392212c70a4ca3",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30,
+}
