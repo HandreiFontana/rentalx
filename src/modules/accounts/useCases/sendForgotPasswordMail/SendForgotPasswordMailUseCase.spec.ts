1ae2698288daf7f8d1876c978f22f4ae781e7f28
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-02-14 12:38:01 -0300

    test(Mail): send forgot password mail test created

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.spec.ts
@@ -0,0 +1,67 @@
+import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
+import { UsersTokensRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory";
+import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
+import { MailProviderInMemory } from "@shared/container/providers/MailProvider/in-memory/MailProviderInMemory";
+import { AppError } from "@shared/errors/AppErrors";
+
+import { SendForgotPasswordMailUseCase } from "./SendForgotPasswordMailUseCase"
+
+let sendForgotPasswordMailUseCase: SendForgotPasswordMailUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let dateProvider: DayjsDateProvider;
+let usersTokensRepositoryInMemory: UsersTokensRepositoryInMemory;
+let mailProvider: MailProviderInMemory;
+
+describe("Send forgot mail", () => {
+
+    beforeEach(() => {
+        usersRepositoryInMemory = new UsersRepositoryInMemory();
+        dateProvider = new DayjsDateProvider();
+        usersTokensRepositoryInMemory = new UsersTokensRepositoryInMemory();
+        mailProvider = new MailProviderInMemory();
+
+        sendForgotPasswordMailUseCase = new SendForgotPasswordMailUseCase(
+            usersRepositoryInMemory,
+            usersTokensRepositoryInMemory,
+            dateProvider,
+            mailProvider,
+        );
+    });
+
+    it("should be able to send a forgot password mail to user", async () => {
+        const sendMail = jest.spyOn(mailProvider, "sendMail");
+
+        await usersRepositoryInMemory.create({
+            driver_license: "295846",
+            email: "bunpatve@apo.lt",
+            name: "Bradley Ramos",
+            password: "1234",
+        });
+
+        await sendForgotPasswordMailUseCase.execute("bunpatve@apo.lt");
+
+        expect(sendMail).toHaveBeenCalled();
+    });
+
+    it("should not be able to send an email if user does not exists", async () => {
+        await expect(
+            sendForgotPasswordMailUseCase.execute("cuw@zoupo.bv")
+        ).rejects.toEqual(new AppError("User does not exists!"));
+    });
+
+    it("should be able to create an user token", async () => {
+        const generateTokenMail = jest.spyOn(usersTokensRepositoryInMemory, "create");
+
+        usersRepositoryInMemory.create({
+            driver_license: "601115",
+            email: "nowsojen@cisid.cv",
+            name: "Edith Dunn",
+            password: "1234",
+        });
+
+        await sendForgotPasswordMailUseCase.execute("nowsojen@cisid.cv");
+
+        expect(generateTokenMail).toBeCalled();
+    });
+
+})
