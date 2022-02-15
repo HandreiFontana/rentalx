3de15e682eca595d7eb0b0bef3545b17b3ff10b6
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-02-15 15:12:42 -0300

    feat(cors): cors installed

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 import express, { Request, Response, NextFunction } from 'express';
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
+import cors from "cors";
 
 import "@shared/container";
 import { AppError } from '@shared/errors/AppErrors';
@@ -21,6 +22,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 
+app.use(cors());
 app.use(router);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
